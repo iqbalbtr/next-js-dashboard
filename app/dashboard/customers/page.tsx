f358f69110b344338de8d695749ca1c8abb01271
haidar <haidarprayoga9b@gmail.com>
2024-09-06 10:25:02 +0700

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <p className="text-3xl text-emerald-500">Customers Page</p>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="bg-teal-700 bg-opacity-50 w-100">
+            <p className="text-blue-700 text-opacity-70 text-center">Invoices Page
+            </p>
+        </div>
+    )
+}
